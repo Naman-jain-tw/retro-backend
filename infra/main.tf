2047d1f55825cdefcec14e12bda17a196eca3530
Naman Jain <naman.jain@thoughtworks.com>
2025-02-12 09:44:20 +0530

    Add cloud run infra. Add deploy to cloud run stage

--- infra/main.tf
+++ infra/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "6.20.0"
+  }
+ }
+}
+
 # Configure the GCP provider
 provider "google" {
   project = "retro-450604"
@@ -10,6 +19,38 @@ resource "google_artifact_registry_repository" "docker_repo" {
   location      = "asia-south1"      
   format        = "DOCKER"           # Specifies that this repo is for Docker images
   description   = "Repository for Docker images"
+}
+
+# Create the Cloud Run Service
+resource "google_cloud_run_service" "default" {
+  name     = "retro-backend-service"
+  location = "asia-south1"
+
+  template {
+    spec {
+      containers {
+        image = "asia-south1-docker.pkg.dev/retro-450604/docker-repo/retro-backend:2151b91dde7c39054c1ea7cf6e547684d6d6c08d"
+        
+        ports {
+          container_port = 8081
+        }
+      }
+    }
+  }
+
+  # Direct 100% of the traffic to the latest revision
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+}
+
+# Allow unauthenticated invocations
+resource "google_cloud_run_service_iam_member" "noauth" {
+  service  = google_cloud_run_service.default.name
+  location = google_cloud_run_service.default.location
+  role     = "roles/run.invoker"
+  member   = "allUsers"
 }
 
 
